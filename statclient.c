b3fdd6b9d986c4aa15b394daeef23461c25adc42
Asuman Aydın <asuman.aydin6709@gmail.com>
2021-10-29 17:25:51 +0300

    Add files via upload

--- statclient.c
+++ statclient.c
@@ -5,7 +5,6 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
@@ -16,35 +15,37 @@
 #include <sys/syscall.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-#define MAX 10
+
 struct mesg_buffer{
 	long mesg_type;
 	char mesg_text[6000];
 } message;
 
+
 int main(int argc, char *argv[])
 {
+    struct timeval current_time, end_time;
     key_t key;
     int msgid;
+    
+    gettimeofday(&current_time, NULL);
+    // printf("seconds : %ld\nmicro seconds : %ld",
+    // current_time.tv_sec, current_time.tv_usec);
+
     key = ftok("/home/asuman/Desktop/os pj1/Project1.pdf", 65);
     msgid = msgget(key, 0666 | IPC_CREAT);
     message.mesg_type = 1;
-    printf("Enter commands: ");
-    while(message.mesg_text != "-1")
+    printf("Enter commands from these options --> avg, count, range, exit.");
+    while(message.mesg_text != "exit")
     {
-        fgets(message.mesg_text,MAX,stdin);
-        if(strstr(message.mesg_text, "max") != NULL || strstr(message.mesg_text, "avg") != NULL || strstr(message.mesg_text, "range") != NULL || strstr(message.mesg_text, "count") != NULL)
-        {
-            msgsnd(msgid, &message, sizeof(message), 0);
-            printf("Data send is : %s \n", message.mesg_text);
-        }
-        else
-        {
-            printf("command is not valid.");
-        }
+        fgets(message.mesg_text,1000,stdin);
+        msgsnd(msgid, &message, sizeof(message), 0);
     }
-    printf("server exited.");
+    printf("Client exited.");
     exit(0);
-    
+    gettimeofday(&end_time, NULL);
+    // printf("seconds : %ld\nmicro seconds : %ld",
+    // current_time.tv_sec, current_time.tv_usec);
     return 0;
+
 }
--- statserver.c
+++ statserver.c
@@ -17,97 +17,120 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #define MAX 10
+
 struct mesg_buffer{
 	long mesg_type;
 	char mesg_text[6000];
 } message;
 
-// void requests()
+void printArray(int A[], int size) 
+{ 
+    int i; 
+    printf(size);
+    printf("\n"); 
+    for (i=0; i < size; i++) 
+        printf("%d ", A[i]); 
+    printf("\n"); 
+} 
+
+float avg(int numberArray[], int size)
+{
+	int average = 0;
+	int total = 0;
+	for(int i = 0; i < size; i++)
+	{
+		total += numberArray[i];
+	}
+	average = total/float(size);
+	return average;
+}
+float avgRange(int numberArray[], int size, int start, int end)
+{
+	int average = 0;
+	int total = 0;
+	for(int i = 0; i < size; i++)
+	{
+		if( start <= numberArray[i] && end >= numberArray[i])
+		{
+			total += numberArray[i];
+		}
+		else
+		{
+			continue;
+		}
+	}
+	average = total/float(size);
+	return average;
+}
+int count(int numberArray[], int size)
+{
+	return size;
+}
+int max(int numberArray[], int size)
+{
+	int max = numberArray[0];
+	for(int i = 1; i < size; i++)
+	{
+		if(max < numberArray[i])
+		{
+			max = numberArray[i];
+		}
+	}
+	return max;
+}
+// int range(int numberArray[], int size)
 // {
-// // 	The request “count” is to find the number all integers in the whole data set.
-// // The request “avg” is to find the average of all integers in the whole data set. The
-// // request “count <start><end>” is to find the number of integers in range [<start>,
-// // <end>], inclusive. The request “avg <start><end>” is to find the average of all
-// // integers in range [<start>, <end>]. The request “range <start> <end> <K>” is to
-// // find the top <K> integers in range [<start>, <end>] in ascending order. In this
-// // request, <K> is the maxiumum number of integers to return. If the [<start>, <end>]
-// // has more integers than <K>, only <K> largest of them will be returned in ascending
-// // sorted order. <K> can be at most 1000, and at least 1.
+
 // }
 
-// void printArray(int A[], int size) 
-// { 
-//     int i; 
-//     printf(size);
-//     printf("\n"); 
-//     for (i=0; i < size; i++) 
-//         printf("%d ", A[i]); 
-//     printf("\n"); 
-// } 
-// int avg()
-// {
 
-// }
-// int count()
-// {
-
-// }
-// int max()
-// {
-
-// }
-// int range()
-// {
-
-// }
 int main(int argc, char *argv[])
 {
-	int noArg = 0;
-	// FILE *intFile;
-
-	// if(argc < 3)
-	// {
-	// 	printf("Number of files should be at least 1\n");
-	// 	exit(1);
-	// }
-	// else if(argc > 12 )
-	// {
-	// 	printf("Number of files should be at most 10\n");
-	// 	exit(1);
-	// }
-
-	noArg = atoi(argv[1]);
-	// if(noArg < 1 || noArg > 10)
-	// {
-	// 	printf("Number of files as N should be between 1 an 10\n");
-	// 	exit(1);
-	// }
-
-	// int * numberArray = (int *)malloc(1000000*sizeof(int));
-	// for(int i = 0; i < noArg; i++)
-	// {
-	// 	intFile = fopen(argv[i+2],"r");
-	// 	if(intFile == NULL)
-	// 	{
-	// 		printf("File cannot be opened\n");
-	// 		exit(1);
-	// 	}
-	// 	else
-	// 	{
-	// 		fscanf(intFile, "%1d", &numberArray[i]);
-	// 	}
-	// }
-
-	// printArray(numberArray, sizeof(numberArray));
-
-
+    int noArg = 0;
+    FILE *intFile;
     key_t key;
     int msgid;
+
+    if(argc < 3)
+    {
+     printf("Number of files should be at least 1\n");
+     exit(1);
+    }
+    else if(argc > 12 )
+    {
+     printf("Number of files should be at most 10\n");
+     exit(1);
+    }
+    noArg = atoi(argv[1]);
+    if(noArg < 1 || noArg > 10)
+    {
+     printf("Number of files as N should be between 1 an 10\n");
+     exit(1);
+    }
+
+    int * numberArray = (int *)malloc(1000000*sizeof(int));
+
+    //BURASI DEĞİŞECEK
+    for(int i = 0; i < noArg; i++)
+    {
+	     intFile = fopen(argv[i+2],"r");
+	     if(intFile == NULL)
+	     {
+	         printf("File cannot be opened\n");
+	         exit(1);
+	     }
+	     else
+	     {
+	         fscanf(intFile, "%1d", &numberArray[i]);
+	     }
+    }
+
+    printArray(numberArray,sizeof(numberArray));
+
     key = ftok("/home/asuman/Desktop/os pj1/Project1.pdf", 65);
     msgid = msgget(key, 0666 | IPC_CREAT);
   
-    // msgrcv to receive message
-    while(message.mesg_text != "-1")
+    while(message.mesg_text != "exit")
     {
 	    msgrcv(msgid, &message, sizeof(message), 1, 0);
 	    pid_t pid;
@@ -149,8 +172,7 @@ int main(int argc, char *argv[])
 			}
 	    }
     }
-    // to destroy the message queue
     msgctl(msgid, IPC_RMID, NULL);
-  
+  	free(numberArray);
     return 0;
 }
